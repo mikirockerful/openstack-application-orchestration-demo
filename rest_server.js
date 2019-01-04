b7cb98ea40bd27746866bf32da671beb55f994c3
Fernando Cepeda <onomatopeyalamenor@gmail.com>
2019-01-04 17:50:40 +0100

    Zk session created. View (path /) renders server's znodeId

--- rest_server.js
+++ rest_server.js
@@ -3,14 +3,51 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var path = require('path');
 var model = require('./model');
+var zookeeper = require('node-zookeeper-client');
+var ZOOKEEPER_ADDRESS = "";
+var znodeId = "";
 
 var app = express();
 
-var port = 8000;
+var port = 80;
 
 model.init(function() {
     console.log('RESTful Service listening in port', port);
-    app.listen(port);
+
+    var client = zookeeper.createClient('localhost:2181');
+    var path = "/members";
+ 
+    client.once('connected', function () {
+        console.log('Connected to the server.');
+ 
+        client.create(path, function (error) {
+            if (error) {
+                console.log('Failed to create node: %s due to: %s.', path, error);
+            } else {
+                console.log('Node: %s is successfully created.', path);
+            }
+
+            client.create(
+                '/members/1',
+                null,
+                3, // EPHEMERAL_SEQUENTIAL
+                function (error, path) {
+                    if (error) {
+                        console.log(error.stack);
+                        return;
+                    }
+
+                    console.log('Node: %s is created.', path);
+                    znodeId = path;
+                    console.log("Path for this server on Zk is: " + znodeId);
+                    //client.close();
+                    app.listen(port);
+                }
+            );
+        });
+    });
+ 
+    client.connect();
 });
 
 app.use(bodyParser.json());
@@ -19,7 +56,7 @@ app.use(methodOverride('_method', {methods: ['GET', 'POST']}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-	res.send('Restaurants REST Service\n');
+    res.send('Zookeeper znode is ' + znodeId);
 })
 
 app.get('/restaurants', function(req, res) {
